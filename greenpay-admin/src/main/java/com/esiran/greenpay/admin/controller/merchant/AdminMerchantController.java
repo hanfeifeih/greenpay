bc8e76a86fd542041aa04f16c2a3d988816381da
admin <admin.git@ishiyou.net>
2020-04-13 23:59:27 +0800

    update merchant edit

--- greenpay-admin/src/main/java/com/esiran/greenpay/admin/controller/merchant/AdminMerchantController.java
+++ greenpay-admin/src/main/java/com/esiran/greenpay/admin/controller/merchant/AdminMerchantController.java
@@ -2,6 +2,7 @@ package com.esiran.greenpay.admin.controller.merchant;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +11,18 @@ public class AdminMerchantController {
     @GetMapping("/list")
     public String list(){
         return "admin/merchant/list";
+    }
+    @GetMapping("/list/{userId}/edit")
+    public String add(@PathVariable String userId){
+        return "admin/merchant/edit";
+    }
+    @GetMapping("/list/{userId}/password")
+    public String password(@PathVariable String userId){
+        return "admin/merchant/password";
+    }
+    @GetMapping("/list/{userId}/settle")
+    public String settle(@PathVariable String userId){
+        return "admin/merchant/settle";
     }
     @GetMapping("/add")
     public String add(){
